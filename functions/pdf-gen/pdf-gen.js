5be64ec896d869d45c97a142cf1ca2b9663892aa
broeker <tim@electriccitizen.com>
2020-06-09 14:29:14 -0500

    Update permissions for pdfkit.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -25,10 +25,10 @@ process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/src/functions/pdf
 // }
 
 exports.handler = function(event, context, callback) {
-  exec('pdftk --version', context.done);
-  // callback(null, {
-  //   statusCode: 200,
-  //   //body: process.env.PATH + "-" + process.env.LAMBDA_TASK_ROOT + '-' + process.env.LD_LIBRARY_PATH
-  //   body: __filename
-  // });
+  const foo = exec('pdftk --version', context.done);
+  callback(null, {
+    statusCode: 200,
+    //body: process.env.PATH + "-" + process.env.LAMBDA_TASK_ROOT + '-' + process.env.LD_LIBRARY_PATH
+    body: foo
+  });
 }
