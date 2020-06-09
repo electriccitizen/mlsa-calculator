0d64b94f777be35c9fab74e6241e70019bbaa674
broeker <tim@electriccitizen.com>
2020-06-09 14:23:03 -0500

    Load PDKit from bin.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -15,9 +15,8 @@ var exec = require('child_process').exec;
 // /var/task
 // /var/task/bin
 
-process.env.PATH = process.env.PATH + ':' + process.env.LAMBDA_TASK_ROOT + '/bin';
-
-process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/bin';
+process.env.PATH = process.env.PATH + ':' + process.env.LAMBDA_TASK_ROOT + '/src/functions/pdf-gen/bin';
+process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/src/functions/pdf-gen/bin';
 
 
 
@@ -27,9 +26,9 @@ process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/bin';
 
 exports.handler = function(event, context, callback) {
   exec('pdftk --version', context.done);
-  callback(null, {
-    statusCode: 200,
-    //body: process.env.PATH + "-" + process.env.LAMBDA_TASK_ROOT + '-' + process.env.LD_LIBRARY_PATH
-    body: __filename
-  });
+  // callback(null, {
+  //   statusCode: 200,
+  //   //body: process.env.PATH + "-" + process.env.LAMBDA_TASK_ROOT + '-' + process.env.LD_LIBRARY_PATH
+  //   body: __filename
+  // });
 }
