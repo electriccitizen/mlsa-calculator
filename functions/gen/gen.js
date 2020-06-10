ed44f4dbc3fa1888f550bc633bbb7fc3d06a6431
broeker <tim@electriccitizen.com>
2020-06-10 16:15:43 -0500

    Test functions.

---
+++ functions/gen/gen.js
@@ -0,0 +1,28 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+var pdfFiller   = require('pdffiller-aws-lambda');
+var sourcePDF = "test.pdf";
+var destinationPDF =  "test/test_complete.pdf";
+var data = {
+  "last_name" : "John",
+  "first_name" : "Doe",
+  "date" : "Jan 1, 2013",
+  "football" : "Off",
+  "baseball" : "Yes",
+  "basketball" : "Off",
+  "hockey" : "Yes",
+  "nascar" : "Off"
+};
+exports.handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
