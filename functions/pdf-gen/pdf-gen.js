1d59d25fdb095faac106bd5703c57b50b6aefb6a
broeker <tim@electriccitizen.com>
2020-06-10 14:58:45 -0500

    Add pdffiller-stream.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -1,11 +1,31 @@
 // functions/pdf-gen/pdf-gen.js
-console.log('foomando')
+const  pdfFiller = require('pdffiller-stream');
+
+const sourcePDF = "test.pdf";
+const data = {
+  "last_name" : "John",
+  "first_name" : "Doe",
+  "date" : "Jan 1, 2013",
+  "football" : "Off",
+  "baseball" : "Yes",
+  "basketball" : "Off",
+  "hockey" : "Yes",
+  "nascar" : "Off"
+};
 
   exports.handler = async function() {
   console.log('fooman')
+    pdfFiller.fillForm( sourcePDF, data)
+      .then((outputStream) => {
+        console.log('i have a stream, yo.')
+        console.log(outputStream)
+        // use the outputStream here;
+        // will be instance of stream.Readable
+      }).catch((err) => {
+      console.log(err);
+    });
     return {
       statusCode: 200,
       body: "Hello world!",
     };
   }
-console.log('foomanidont')
