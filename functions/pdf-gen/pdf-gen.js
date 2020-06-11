f5b3b40ea703199271077e97ea1898797a7f8ba2
broeker <tim@electriccitizen.com>
2020-06-11 11:17:14 -0500

    Gen test.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -1,13 +1,28 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
 // Set the PATH and LD_LIBRARY_PATH environment variables.
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
 exports.handler = function (event, context,callback) {
-  console.log(spawn('pdftk --version', context.done));
+  //console.log(exec('pdftk --version', context.done));
+
+  exec("pdftk --version", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+
+
+
   callback(null, {
     statusCode: 200,
     //body: process.env.PATH + "-" + process.env.LAMBDA_TASK_ROOT + '-' + process.env.LD_LIBRARY_PATH
