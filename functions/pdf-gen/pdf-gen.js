1f6e31c5dc18be398301ad3b308fa7a0d8a1c6ab
broeker <tim@electriccitizen.com>
2020-06-11 13:25:06 -0500

    Path updates.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -22,7 +22,7 @@ process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/src/functions/pdf
 exports.handler = function (event, context,callback) {
   //console.log(exec('pdftk --version', context.done));
   console.log(process.env.PATH + '--' + process.env.LAMBDA_TASK_ROOT + '--' + process.env.LD_LIBRARY_PATH)
-  exec("pdftk --version", (error, stdout, stderr) => {
+  exec("./bin/pdftk --version", (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
       return;
