d28e202bc643055c185affbf3b52e0f8ea4bfd96
broeker <tim@electriccitizen.com>
2020-06-11 13:54:11 -0500

    Path updates.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -1,7 +1,7 @@
 'use strict';
 const  pdfFiller = require('pdffiller-stream');
-
-var exec = require('child_process').exec;
+const sourcePDF = "test.pdf";
+//var exec = require('child_process').exec;
 // Set the PATH and LD_LIBRARY_PATH environment variables.
 // process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 // process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
