85571d0a86bfcfc4387e4bdaa5d9e790a1f2401e
broeker <tim@electriccitizen.com>
2020-06-11 13:09:51 -0500

    Gen test.

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -3,8 +3,10 @@
 var exec = require('child_process').exec;
 
 // Set the PATH and LD_LIBRARY_PATH environment variables.
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
-process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
+// process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
+// process.env['LD_LIBRARY_PATH'] = process.env['LAMBDA_TASK_ROOT'] + '/bin';
+process.env.PATH = process.env.PATH + ':' + process.env.LAMBDA_TASK_ROOT + '/src/functions/pdf-gen/bin';
+process.env.LD_LIBRARY_PATH = process.env.LAMBDA_TASK_ROOT + '/src/functions/pdf-gen/bin';
 
 exports.handler = function (event, context,callback) {
   //console.log(exec('pdftk --version', context.done));
