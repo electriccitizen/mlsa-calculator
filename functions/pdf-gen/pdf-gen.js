31aef2c635f9dbe8150b6da2707f76f0aa2f50b6
broeker <tim@electriccitizen.com>
2020-09-25 11:40:57 -0500

    fix source path for fillable pdf

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -4,7 +4,8 @@ const sourcePDF =
   process.env.LOCAL_ENV === true
     ? process.env.LAMBDA_TASK_ROOT + "/functions/pdf-gen/mcsg-fillable.pdf"
     : process.env.LAMBDA_TASK_ROOT + "/src/functions/pdf-gen/mcsg-fillable.pdf"
-
+const sourcePDF =
+  "/home/broeker/Projects/mlsa-calculator/functions/pdf-gen/mcsg-fillable.pdf"
 const { Base64Encode } = require("base64-stream")
 // const flatten = require("flat").flatten
 // var exec = require("child_process").exec
