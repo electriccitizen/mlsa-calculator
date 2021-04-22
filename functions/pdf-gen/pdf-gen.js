e5268f203050f122364d4ccc66fbefc04c273875
Patryk Bończoszek <patryk.bonczoszek@droptica.pl>
2021-04-22 10:22:29 +0200

    test path to temp dir

--- functions/pdf-gen/pdf-gen.js
+++ functions/pdf-gen/pdf-gen.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const pdftk = require('node-pdftk')
+const path = require('path');
 const { processData } = require('./processors/process')
 
 // Set the root path
@@ -16,7 +17,8 @@ process.env.LD_LIBRARY_PATH = `${ROOT}/src/bin`
 
 // Set the directory where temporary files are stored
 pdftk.configure({
-  tempDir: `${ROOT}/node-pdftk-tmp`
+  // tempDir: `${ROOT}/node-pdftk-tmp`
+  tempDir: path.join(__dirname, './node-pdftk-tmp')
 })
 
 // Sources pdf files
@@ -63,26 +65,31 @@ const generatePdf = values => {
 
 exports.handler = function (event, context, callback) {
   if (event.body !== null && event.body !== undefined) {
-    const formData = JSON.parse(event.body)
-    const data = processData(formData, pdfs)
+    // const formData = JSON.parse(event.body)
+    // const data = processData(formData, pdfs)
 
-    generatePdf(data)
-      .then(buffer => {
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(buffer.toString('base64')),
-        })
-      })
-      .catch(error => {
-        callback(null, {
-          statusCode: 500,
-          body: JSON.stringify(error),
-        })
-      })
-  } else {
     callback(null, {
-      statusCode: 400,
-      body: JSON.stringify("No data body."),
+      statusCode: 200,
+      body: JSON.stringify({ pdftk: pdftk.input(), env: process.env }),
     })
+
+  //   generatePdf(data)
+  //     .then(buffer => {
+  //       callback(null, {
+  //         statusCode: 200,
+  //         body: JSON.stringify(buffer.toString('base64')),
+  //       })
+  //     })
+  //     .catch(error => {
+  //       callback(null, {
+  //         statusCode: 500,
+  //         body: JSON.stringify(error),
+  //       })
+  //     })
+  // } else {
+  //   callback(null, {
+  //     statusCode: 400,
+  //     body: JSON.stringify("No data body."),
+  //   })
   }
 }
