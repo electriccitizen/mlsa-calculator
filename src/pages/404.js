8fbdedbce5df035df9dfa0596e62e241b960da4f
broeker <tim@electriccitizne.com>
2021-08-26 11:56:56 -0500

    add layout to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from "react"
+import { Box } from "@chakra-ui/react"
+
+import { PageLayout } from "../layout/PageLayout"
+
 export default function NotFound() {
   return (
-    <h1>Not found</h1>
+      <PageLayout>
+        <Box mb="8">
+            <h1>Not found</h1>
+        </Box>
+      </PageLayout>
   )
 }
+
