846c5cec16cb3c51883af3f5722e591c3d722823
broeker <tim@electriccitizen.com>
2022-08-04 14:33:39 -0500

    MLSA-00: troubleshoot pdfgen.

--- src/pages/child-support/calculator.js
+++ src/pages/child-support/calculator.js
@@ -104,53 +104,53 @@ export default function Calculator() {
             <TermsOfUse />
             <InitiateInterview />
             <BasicInformation />
-            <OtherParent />
-            <NumberChildren />
-            <EnterChildren />
-            <OtherChildren />
-            <EnterMyOtherChildren />
-            {(documents === "both" || documents === "worksheets") && (
-              <>
-                <OtherChildrenSecondary />
-                <EnterMyOtherChildrenSecondary />
-              </>
-            )}
-            <Employment />
-            <CurrentJob />
-            <OtherJobs />
-            <EnterOtherJobs />
-            <OtherIncome />
-            <TaxableIncome />
-            <NonTaxableIncome />
-            <ChildExpenses />
-            <AllowableDeductions />
-            <OtherAllowableDeductions />
-            <StandardOfLiving />
-            {(documents === "both" || documents === "worksheets") && (
-              <>
-                <CurrentJobSecondary />
-                <EnterOtherJobsSecondary />
-                <OtherIncomeSecondary />
-                <TaxableIncomeSecondary />
-                <NonTaxableIncomeSecondary />
-                <ChildExpensesSecondary />
-                <AllowableDeductionsSecondary />
-                <OtherAllowableDeductionsSecondary />
-                <StandardOfLivingSecondary />
-              </>
-            )}
-            <ParentingDays />
-            {(documents === "both" || documents === "affadavit") && (
-              <>
-                <FinancialAffadavitOne />
-                <Schools />
-                <OtherSchools />
-                <HealthInsurance />
-                <HealthInsurancePolicies />
-                <FinancialAffadavitTwo />
-                <FinancialAffadavitThree />
-              </>
-            )}
+            {/*<OtherParent />*/}
+            {/*<NumberChildren />*/}
+            {/*<EnterChildren />*/}
+            {/*<OtherChildren />*/}
+            {/*<EnterMyOtherChildren />*/}
+            {/*{(documents === "both" || documents === "worksheets") && (*/}
+            {/*  <>*/}
+            {/*    <OtherChildrenSecondary />*/}
+            {/*    <EnterMyOtherChildrenSecondary />*/}
+            {/*  </>*/}
+            {/*)}*/}
+            {/*<Employment />*/}
+            {/*<CurrentJob />*/}
+            {/*<OtherJobs />*/}
+            {/*<EnterOtherJobs />*/}
+            {/*<OtherIncome />*/}
+            {/*<TaxableIncome />*/}
+            {/*<NonTaxableIncome />*/}
+            {/*<ChildExpenses />*/}
+            {/*<AllowableDeductions />*/}
+            {/*<OtherAllowableDeductions />*/}
+            {/*<StandardOfLiving />*/}
+            {/*{(documents === "both" || documents === "worksheets") && (*/}
+            {/*  <>*/}
+            {/*    <CurrentJobSecondary />*/}
+            {/*    <EnterOtherJobsSecondary />*/}
+            {/*    <OtherIncomeSecondary />*/}
+            {/*    <TaxableIncomeSecondary />*/}
+            {/*    <NonTaxableIncomeSecondary />*/}
+            {/*    <ChildExpensesSecondary />*/}
+            {/*    <AllowableDeductionsSecondary />*/}
+            {/*    <OtherAllowableDeductionsSecondary />*/}
+            {/*    <StandardOfLivingSecondary />*/}
+            {/*  </>*/}
+            {/*)}*/}
+            {/*<ParentingDays />*/}
+            {/*{(documents === "both" || documents === "affadavit") && (*/}
+            {/*  <>*/}
+            {/*    <FinancialAffadavitOne />*/}
+            {/*    <Schools />*/}
+            {/*    <OtherSchools />*/}
+            {/*    <HealthInsurance />*/}
+            {/*    <HealthInsurancePolicies />*/}
+            {/*    <FinancialAffadavitTwo />*/}
+            {/*    <FinancialAffadavitThree />*/}
+            {/*  </>*/}
+            {/*)}*/}
             <CompleteApp state={appState.complete} pdf={appState.pdf} />
           </RulesProvider>
         </MultiStepsLayout>
