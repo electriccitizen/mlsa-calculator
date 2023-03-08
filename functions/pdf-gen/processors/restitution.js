1e938cb1b377a8507caa7e39c0e670fe8d4fb46e
broeker <tim@electriccitizen.com>
2023-03-07 20:01:53 -0600

    fixes

--- functions/pdf-gen/processors/restitution.js
+++ functions/pdf-gen/processors/restitution.js
@@ -114,8 +114,10 @@ const calcRestitution = form => {
               // const amtPaid = getValueAsNumber(item, ["amt"])
               const amtPaidInsurance = getValueAsNumber(item, ["amtInsurance"])
 
+              console.log('TOTAL EXPENSE: ' . totalExpense)
+              console.log('AMT PAID BY INS: ' . amtPaidInsurance)
               const totalAmountPaid = (totalExpense - amtPaidInsurance)
-              console.log(totalAmountPaid)
+              console.log("Total amount paid: " . totalAmountPaid)
 
               // console.log( getValueAsNumber(item, ["expense"]))
               // console.log( getValueAsNumber(item, ["amt"]))
